fc2bdf59f9412df3229872d200891cb6ce575f53
Nick Perkins <nickperkins@protonmail.com>
2016-05-10 15:32:56 -0400

    Finished project

--- SwiftMissionBriefing/ViewController.swift
+++ SwiftMissionBriefing/ViewController.swift
@@ -9,13 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var agentNameTextField: UITextField!
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var missionBriefingLabel: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        agentNameTextField.text = ""
+        greetingLabel.text = ""
+        passwordTextField.text = ""
+        missionBriefingLabel.text = ""
+    }
+    
+    @IBAction func authenticateButton(sender: UIButton) {
+        
+        if agentNameTextField.isFirstResponder() {
+            agentNameTextField.resignFirstResponder()
+        }
+        
+        if (!agentNameTextField.text!.isEmpty) {
+            var firstName = agentNameTextField.text?.componentsSeparatedByString(" ").first
+            var lastName =  agentNameTextField.text?.componentsSeparatedByString(" ").last
+            var string1 = "GOOD EVENING, AGENT "
+            var string2 = "!"
+            var specialGreeting = string1 + lastName! + string2
+            greetingLabel.text = specialGreeting
+        
+        var fullMissionText = "This mission will be an arduous one, fraught with peril. You will cover much strange and unfamiliar territory. Should you choose to accept this mission, Agent \(lastName!), you will certainly be disavowed, but you will be doing your country a great service. This message will self destruct in 5 seconds."
+            
+      missionBriefingLabel.text = fullMissionText
+            
+        view.backgroundColor = .greenColor()
+        missionBriefingLabel.backgroundColor = .greenColor()
+            
+        }else{
+            
+        view.backgroundColor = .redColor()
+        missionBriefingLabel.backgroundColor = .redColor()
+        
     }
 
-    override func didReceiveMemoryWarning() {
+    func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
@@ -23,3 +60,4 @@ class ViewController: UIViewController {
 
 }
 
+}
